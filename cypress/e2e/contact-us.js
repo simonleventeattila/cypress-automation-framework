5521fa729f4d726b46fe464c63ae3c6f05a4e80d
simonleventeattila <simonleventeattila@gmail.com>
2023-03-08 10:34:16 +0200

    fix: typos

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,16 +1,16 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact us form via Webdriveruni", ()=>{
-    it("Should be able to submitt a succseful submission via contact us form",()=>{
+    it("Should be able to submit a successful submission via contact us form",()=>{
         //code
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         cy.get('[name="first_name"]').type("Theodora");
         cy.get('[name="last_name"]').type("Mathe");
         cy.get('[name="email"]').type("teodora@gmail.com");
-        cy.get('textarea.feedback-input').type("Whatevere comment");
+        cy.get('textarea.feedback-input').type("Whatever comment");
         cy.get('[type="submit"]').click();
 
-    it.only("Should not be able to submitt a succseful submission via contact us form as all fields are required",()=>{
+    it.only("Should not be able to submit a successful submission via contact us form as all fields are required",()=>{
     });
     cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
     cy.get('[name="first_name"]').type("Theodora");
