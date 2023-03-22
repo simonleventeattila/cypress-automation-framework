af45c8190a1c7d53b933b307d70d0c6b4fb99e4c
simonleventeattila <simonleventeattila@gmail.com>
2023-03-22 16:57:18 +0200

    changed to xpath selecto - reference error

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,14 +1,17 @@
 /// <reference types="Cypress" />
+//// <reference types="cypress-xpath" />
 
 describe("Test Contact us form via Automation Test store", ()=>{
     it("Should be able to submit a successful submission via contact us form",()=>{
        //code
         cy.visit("https://automationteststore.com/", );
         cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.xpath("a[contains(@href,'contact')]").click
+      //a[contains(@href,'contact')]
         cy.get('#ContactUsFrm_first_name').type("Max Musterman");
         cy.get('#ContactUsFrm_email').type("asd@asd.com");
         cy.get('#ContactUsFrm_enquiry').type("What is life?");
-        cy.get('.col-md-6 > .btn').click();
+      //  cy.get('.col-md-6 > .btn').click();
     });
 
     it.only("Should be able to submit a successful submission via contact us form",()=>{
