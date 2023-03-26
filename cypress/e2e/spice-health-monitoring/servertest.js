b0e63e391d29af54f51ccf5923f7dd9127af7ce9
IDEAPAD3\F0RCE <simon.leventeattila@gmail.com>
2023-03-26 23:07:14 +0300

    feature: servertest for spice services

---
+++ cypress/e2e/spice-health-monitoring/servertest.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+
+describe("Check that services are running in PROD", ()=>{
+    it("Check that QCS is up and running",()=>{
+        //code
+        cy.visit("https://mall.industry.siemens.com/spice/quotecreation-service/servertest");
+        cy.contains("succeeded");
+
+    });
+
+    it("Check that QLM is up and running",()=>{
+        //code
+        cy.visit("https://mall.industry.siemens.com/spice/qlm-service/servertest");
+        cy.contains("succeeded");
+    });
+
+    it("Check that IQ is up and running",()=>{
+        //code
+        cy.visit("https://mall.industry.siemens.com/spice/csk/servertest");
+        cy.contains("succeeded");
+    });
+
+});
+
