8cee8279c1d4a915335c08fff5f2486f90c01bdb
IDEAPAD3\F0RCE <simon.leventeattila@gmail.com>
2023-10-18 23:45:38 +0300

    All methods of described

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -2,28 +2,42 @@
 /// <reference types='@cypress/xpath' />
 
 
-describe("Verifying variables, cypress commands and jquery commands", ()=>{
-    it.only("Click on product/category",()=>{
-      cy.visit("https://automationteststore.com/");
-      cy.get("a[href*='product/category&path=']").contains("Makeup").click();
-     //Following commnad will fail
-     const header = cy.get("h1 .maintext");
-     cy.log(header);
+describe("Verifying variables, cypress commands and jquery commands", () => {
+  it("Navigating to specific product pages", () => {
+      cy.visit("https://automationteststore.com/")
+      //The following will fail
+      // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
+      // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
+      // makeupLink.click();
+      // skincareLink.click();
 
-    //  cy.get("h1 .maintext").then(($headerText)=> {
-    //     const headerText = $headerText.text()
-    //     cy.log("found header text:" + headerText)
-    //  })
-    });
-   
-    it("Click on the first item using item header",()=>{
-      cy.visit("https://automationteststore.com/");
-      cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
-    });
- 
+      //The following will pass
+      // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
+      // makeupLink.click();
+      // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
+      // skincareLink.click();
 
-    
+      //Recommended Approach
+      cy.get("a[href*='product/category&path=']").contains("Skincare").click()
+      cy.get("a[href*='product/category&path=']").contains("Makeup").click()
+  });
+  it("Navigating to specific product pages", () => {
+      cy.visit("https://automationteststore.com/")
+      cy.get("a[href*='product/category&path=']").contains("Makeup").click()
 
-    
+      //Following code will fail
+      // const header = cy.get("h1 .maintext");
+      // cy.log(header.text())
+
+      cy.get("h1 .maintext").then(($headerText) => {
+          const headerText = $headerText.text()
+          cy.log("Found header text: " + headerText)
+          expect(headerText).is.eq('Makeup')
+      })
+  });
 
 })
+
+    
+
+    
