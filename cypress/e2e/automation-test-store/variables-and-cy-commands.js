e0c6cea311f9bab12babba07ebd63ba9b56e27e4
IDEAPAD3\F0RCE <simon.leventeattila@gmail.com>
2023-10-18 23:38:39 +0300

    Add correct and incorrect variables

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -3,10 +3,24 @@
 
 
 describe("Verifying variables, cypress commands and jquery commands", ()=>{
+    it.only("Click on product/category",()=>{
+      cy.visit("https://automationteststore.com/");
+      cy.get("a[href*='product/category&path=']").contains("Makeup").click();
+     //Following commnad will fail
+     const header = cy.get("h1 .maintext");
+     cy.log(header);
+
+    //  cy.get("h1 .maintext").then(($headerText)=> {
+    //     const headerText = $headerText.text()
+    //     cy.log("found header text:" + headerText)
+    //  })
+    });
+   
     it("Click on the first item using item header",()=>{
       cy.visit("https://automationteststore.com/");
       cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
     });
+ 
 
     
 
